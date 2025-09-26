6458d156e47e6fa3d19b8b2ae2386972f6587671
byronpastor-bitsis <byronpastor.bitsis@gmail.com>
2025-09-26 10:24:41 -0600

    feat: add new drawable resources and update configurations
    - Add multiple XML drawable files for icons and backgrounds
    - Update Android SDK versions and Gradle plugin
    - Add new color resources and clean up strings
    - Fix bottom navigation menu reference

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,14 +6,14 @@ buildscript {
     }
     
     dependencies {
-        classpath("com.android.tools.build:gradle:8.1.3")
+        classpath("com.android.tools.build:gradle:8.12.3")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.5")
     }
 }
 
 plugins {
-    id("com.android.application") version "8.1.3" apply false
+    id("com.android.application") version "8.12.3" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
     id("com.google.dagger.hilt.android") version "2.48.1" apply false
 }
